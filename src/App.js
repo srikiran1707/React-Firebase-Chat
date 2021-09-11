1bbaa69d642d0b128585ebd811c73f57b2f46d66
srikiran <68954979+srikiran1707@users.noreply.github.com>
2021-09-11 20:09:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -53,7 +53,7 @@ function SignIn(){
 
 function SignOut(){
   return auth.currentUser &&(
-<button className="sign-out" onClick={()=>auth.signOut}>Exit</button>
+<button className="sign-out" onClick={()=>auth.signOut()}>Exit</button>
   )
 }
 
